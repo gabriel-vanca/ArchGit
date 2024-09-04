fef2b635b8d8492ec553fdd1575400cdb4c97c7b
Gabriel Vanca <gabriel@vanca.co.uk>
2024-09-04 19:36:20 +0100

    Colours

--- RepoZ.Ipc/DefaultIpcEndpoint.cs
+++ RepoZ.Ipc/DefaultIpcEndpoint.cs
@@ -1,7 +1,14 @@
 ﻿namespace RepoZ.Ipc
 {
+<<<<<<< HEAD
 	public class DefaultIpcEndpoint : IIpcEndpoint
 	{
 		public string Address => "tcp://localhost:18181";
 	}
+=======
+    public class DefaultIpcEndpoint : IIpcEndpoint
+    {
+        public string Address => "tcp://localhost:18182";
+    }
+>>>>>>> 3895eb2 (Colours)
 }

14dc727119c72bb0142f795b5feb3e7b5e6a56dc
Gabriel Vanca <gabriel@vanca.co.uk>
2024-09-05 05:30:42 +0100

    fixing the port debug issue

--- RepoZ.Ipc/DefaultIpcEndpoint.cs
+++ RepoZ.Ipc/DefaultIpcEndpoint.cs
@@ -2,6 +2,10 @@
 {
     public class DefaultIpcEndpoint : IIpcEndpoint
     {
+#if DEBUG
+        public string Address => "tcp://localhost:18183";
+#else
         public string Address => "tcp://localhost:18182";
+#endif
     }
 }
--- Specs/Ipc/TestIpcEndpoint.cs
+++ Specs/Ipc/TestIpcEndpoint.cs
@@ -4,6 +4,11 @@ namespace Specs.Ipc
 {
     class TestIpcEndpoint : IIpcEndpoint
     {
+
+#if DEBUG
+        public string Address => "tcp://localhost:18183";
+#else
         public string Address => "tcp://localhost:18182";
+#endif
     }
 }

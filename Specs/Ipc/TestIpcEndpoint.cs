22fafcaf20c2a7ecee10f9fe6ad8fc0cee4d03fb
Gabriel Vanca <gabriel@vanca.co.uk>
2024-09-05 05:30:42 +0100

    fixing the port debug issue

--- Specs/Ipc/TestIpcEndpoint.cs
+++ Specs/Ipc/TestIpcEndpoint.cs
@@ -7,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace Specs.Ipc
 {
+<<<<<<< HEAD
 	class TestIpcEndpoint : IIpcEndpoint
 	{
 		public string Address => "tcp://localhost:18182";
 	}
 }
+=======
+    class TestIpcEndpoint : IIpcEndpoint
+    {
+
+#if DEBUG
+        public string Address => "tcp://localhost:18183";
+#else
+        public string Address => "tcp://localhost:18182";
+#endif
+    }
+}
+>>>>>>> 14dc727 (fixing the port debug issue)
